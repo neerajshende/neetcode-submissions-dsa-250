a6008df134821b41ecf8994c2961a61974c0cb80
Neeraj Shende <neerajshende100@gmail.com>
2026-04-17 01:53:40 +0530

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums, val) {
+        let left = 0; let right = nums.length-1;
+        while(left<=right) {
+
+            if(nums[left]=== val ) {
+                while(left<=right && nums[right]=== val) {
+                    right--;
+                }
+                if(left<right) {
+                    [nums[left],nums[right]]= [nums[right], nums[left]];
+                    right--;
+                    left++;
+                }
+            } else {
+                left++;
+            }
+        }
+        return right+1;
+    }
+}
