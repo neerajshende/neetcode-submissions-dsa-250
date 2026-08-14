d7a33387ee46c70b698a8d1f7a779c7b7b980dc3
Neeraj Shende <neerajshende100@gmail.com>
2026-08-15 04:15:15 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length !== t.length) {
+            return false;
+        }
+        let charMap = {};
+        for(let char of s) {
+            charMap[char] = charMap[char]?charMap[char]+1 : 1;
+        }
+        for(let char of t){
+            if(!charMap[char]) {
+                return false
+            } 
+            charMap[char]--;
+        }
+        return true;
+    }
+}
